2f0a02c61ada51dd1ade513a654d9abce70d6047
jleestromer <Eugene.Lucas@o2aicorp.com>
2025-08-12 05:17:42 -0700

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',         // <-- Add this line for relative paths to assets
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Bind to all available network interfaces
-    port: 5173, // Default port, change if necessary
+    host: '0.0.0.0',  // Bind to all available network interfaces
+    port: 5173,       // Default port, change if necessary
   },
 })
-
- 
